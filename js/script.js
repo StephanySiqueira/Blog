b8115f622c57479e85e6b93fbe2d78b499574df3
stephany-siqueira <rm98258@fiap.com.br>
2023-09-29 09:32:12 -0300

    Commit inicial

--- js/script.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-    const heartIcon = document.querySelectorAll('.heart-icon');
-
-    heartIcon.forEach(function (icon) {
-        icon.addEventListener('click', () => {
-            icon.classList.toggle('active');
-        });
-    });
-}, 500);
